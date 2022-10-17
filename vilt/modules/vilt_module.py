c236272914e9852c49d36a7897a3d3bb2baf2cc4
rishubh <rishubhngp@gmail.com>
2022-10-16 23:20:48 -0400

    cleaning up vilt module

--- vilt/modules/vilt_module.py
+++ vilt/modules/vilt_module.py
@@ -27,18 +27,6 @@ class ViLTransformerSS(pl.LightningModule):
         self.pooler = heads.Pooler(config["hidden_size"])
         self.pooler.apply(objectives.init_weights)
 
-        # if config["loss_names"]["mlm"] > 0:
-        #     self.mlm_score = heads.MLMHead(bert_config)
-        #     self.mlm_score.apply(objectives.init_weights)
-
-        # if config["loss_names"]["itm"] > 0:
-        #     self.itm_score = heads.ITMHead(config["hidden_size"])
-        #     self.itm_score.apply(objectives.init_weights)
-
-        # if config["loss_names"]["mpp"] > 0:
-        #     self.mpp_score = heads.MPPHead(bert_config)
-        #     self.mpp_score.apply(objectives.init_weights)
-
         # ===================== Downstream ===================== #
         if (
             self.hparams.config["load_path"] != ""
@@ -115,79 +103,13 @@ class ViLTransformerSS(pl.LightningModule):
             x, _attn = blk(x, mask=co_masks)
 
         x = self.transformer.norm(x)
-        text_feats, image_feats = (
-            x[:, : text_embeds.shape[1]],
-            x[:, text_embeds.shape[1] :],
-        )
         cls_feats = self.pooler(x)
         return cls_feats
-
-        # ret = {
-        #     # "text_feats": text_feats,
-        #     # "image_feats": image_feats,
-        #     "cls_feats": cls_feats,
-        #     # "raw_cls_feats": x[:, 0],
-        #     # "image_labels": image_labels,
-        #     # "image_masks": image_masks,
-        #     # "text_labels": text_labels,
-        #     # "text_ids": text_ids,
-        #     # "text_masks": text_masks,
-        #     # "patch_index": patch_index,
-        # }
-
-        # return ret
 
     def forward(self, batch):
 
         logits = self.infer(batch)
         return self.vqa_classifier(logits)
-
-        #ret = dict()
-        #if len(self.current_tasks) == 0:
-        #    res = self.infer(batch)
-        #     print("about to return")
-        #     ret.update(res)
-        #     return ret
-
-        # # Visual Question Answering
-        # if "vqa" in self.current_tasks:
-        #     ret.update(objectives.compute_vqa(self, batch))
-
-        # return ret
-
-    def training_step(self, batch, batch_idx):
-        vilt_utils.set_task(self)
-        output = self(batch)
-        total_loss = sum([v for k, v in output.items() if "loss" in k])
-
-        return total_loss
-
-    def training_epoch_end(self, outs):
-        vilt_utils.epoch_wrapup(self)
-
-    def validation_step(self, batch, batch_idx):
-        vilt_utils.set_task(self)
-        output = self(batch)
-
-    def validation_epoch_end(self, outs):
-        vilt_utils.epoch_wrapup(self)
-
-    def test_step(self, batch, batch_idx):
-        vilt_utils.set_task(self)
-        output = self(batch)
-        ret = dict()
-
-        if self.hparams.config["loss_names"]["vqa"] > 0:
-            ret.update(objectives.vqa_test_step(self, batch, output))
-
-        return ret
-
-    def test_epoch_end(self, outs):
-        model_name = self.hparams.config["load_path"].split("/")[-1][:-5]
-
-        if self.hparams.config["loss_names"]["vqa"] > 0:
-            objectives.vqa_test_wrapup(outs, model_name)
-        vilt_utils.epoch_wrapup(self)
 
     def configure_optimizers(self):
         return vilt_utils.set_schedule(self)
